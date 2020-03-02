8a195ab00096e287a64d22c7c480aeee7399431e
Patrick Blanc <perso@patrickblanc.net>
2020-03-02 15:48:27 +0100

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get '/ask', to:'questions#ask', as: 'questions_ask'
+  get '/answer', to:'questions#answer', as: 'questions_answer'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
